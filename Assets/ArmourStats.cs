f917bb6191207f859d1438b5c0d7a586d369ed79
Josh <joshuaeric.malcolm@gmail.com>
2020-09-07 10:51:44 +1200

    Prototype project

---
+++ Assets/ArmourStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmourStats : PlayerStats
+{
+    public PlayerStats stats;
+    public int legsIndex;
+    public int chestIndex;
+    public int headIndex;
+
+
+
+}
