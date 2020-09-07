0a569560bcefab6d5e0e77a01e1ad63fb371f868
Josh <joshuaeric.malcolm@gmail.com>
2020-09-07 22:06:23 +1200

    Update Project Prototype

---
+++ Assets/Scripts/ArmourStats.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmourStats : MonoBehaviour
+{
+    public string armourType;
+    [Range(0,1)]
+    public int armourIndex;
+    [SerializeField] private Player player;
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+        armourIndex = 1;
+    }
+
+
+    public int armourWeightStat()
+    {
+        switch (armourIndex)
+        {
+            case 1:
+                armourType = "Heavy";
+                return 20;
+            default:
+                armourType = "Light";
+                return 10;
+        }
+    }
+    public int armourSpeedReduceStat()
+    {
+        switch (armourIndex)
+        {
+            case 1:
+                armourType = "Heavy";
+                return 6;
+            default:
+                armourType = "Light";
+                return 3;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            player.hasArmour = !player.hasArmour;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            armourIndex = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            armourIndex = 1;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] public int maxJumps;
+
+
+    [SerializeField] public float horizontal;
+    [SerializeField] private float horizontalInput;
+
+    [SerializeField] private bool leftKey;
+    [SerializeField] private bool rightKey;
+    [SerializeField] public int canJump;
+
+    [SerializeField] private Player player;
+    private void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    public void HorizontalInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        horizontal = (horizontalInput);
+
+        if (horizontalInput < 0)
+        {
+            leftKey = true;
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else
+        {
+            leftKey = false;
+        }
+        if (horizontalInput > 0)
+        {
+            rightKey = true;
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        else
+        {
+            rightKey = false;
+        }
+    }
+
+    public void JumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            canJump -= 1;
+            if (canJump < 0)
+            {
+                canJump = 0;
+            }
+        }
+        if (canJump > maxJumps && player.grounded == true)
+        {
+            canJump = maxJumps;
+        }
+    }
+
+    public void AttackInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.attack.JabAttack();
+        }
+    }
+}
