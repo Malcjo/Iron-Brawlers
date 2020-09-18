16e6dcfc79dc3e50f29ca855df1ceac7c8569324
Peter <mcdermpete@gmail.com>
2020-09-18 16:21:15 +1200

    Grey Box Menu Implemented

---
+++ Assets/Scripts/UIScripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+    
+{
+    public GameObject titleScreen;
+    public GameObject mainMenuScreen;
+    private void Start()
+    {
+        titleScreen.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            titleScreen.SetActive(false);
+        }
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
