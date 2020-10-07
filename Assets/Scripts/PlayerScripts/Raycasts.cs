fa67e2be84bd6ec441699141ad4b8da36cea1c59
Josh <joshuaeric.malcolm@gmail.com>
2020-10-08 11:51:30 +1300

    walls sorta work, don't stop movement yet

--- Assets/Scripts/PlayerScripts/Raycasts.cs
+++ Assets/Scripts/PlayerScripts/Raycasts.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Raycasts : MonoBehaviour
 {
 
-    private Vector3 rayCastOrigin;
-    public Vector3 rayCastOffset;
-
     private float groundCheckRayLength = 0.4f;
-    private float headCheckRayLength = 0.7f;
+    private float headCheckRayLength = 0.5f;
+    private float sideCheckRayLength = 0.2f;
 
 
     public float distanceToGround;
     public float distanceToCeiling;
+    public float distanceToRight;
+    public float distanceToLeft;
 
     Checker checker;
     Player player;
@@ -27,12 +27,68 @@ public class Raycasts : MonoBehaviour
     {
         DownRays();
         UpRays();
+        LeftRays();
+        RightRays();
+
     }
+
+    private void LeftRays()
+    {
+        RaycastHit hit;
+        Vector3 rayCastOrigin = transform.position;
+        Debug.DrawRay(rayCastOrigin, Vector3.left * sideCheckRayLength, Color.red);
+        if (Physics.Raycast(rayCastOrigin, Vector3.left, out hit, sideCheckRayLength))
+        {
+            if(checker.jumping == true || checker.falling == true)
+            {
+                if (hit.collider.CompareTag("Ground"))
+                {
+                    HitLeft(hit);
+                }
+            }
+        }
+    }
+    void HitLeft(RaycastHit hit)
+    {
+        distanceToLeft = hit.distance;
+        player.rb.velocity = new Vector3(player.rb.velocity.x, 0, 0);
+        if (distanceToLeft >= 0 && distanceToLeft <= 0.37f)
+        {
+            transform.position = new Vector3(hit.point.x + 0.25f, hit.point.y, 0);
+        }
+        distanceToLeft = hit.distance;
+    }
+    //--------------------------------------------------------------------------------
+    private void RightRays()
+    {
+        RaycastHit hit;
+        Vector3 rayCastOrigin = transform.position;
+        Debug.DrawRay(rayCastOrigin, Vector3.right * sideCheckRayLength, Color.red);
+        if (Physics.Raycast(rayCastOrigin, Vector3.right, out hit, sideCheckRayLength)) 
+        {
+            if (checker.jumping == true || checker.falling == true) 
+            {
+                HitRight(hit);
+            } 
+        }
+    }
+    void HitRight(RaycastHit hit)
+    {
+        distanceToRight = hit.distance;
+        player.rb.velocity = new Vector3(player.rb.velocity.x, 0, 0);
+        if (distanceToRight >= 0 && distanceToRight <= 0.37f)
+        {
+            transform.position = new Vector3(hit.point.x - 0.25f, hit.point.y, 0);
+        }
+        distanceToRight = hit.distance;
+    }
+    //--------------------------------------------------------------------------------
     private void UpRays()
     {
-        Debug.DrawRay(transform.position, Vector3.up * headCheckRayLength, Color.red);
         RaycastHit hit;
-        if (Physics.Raycast(rayCastOrigin, Vector3.up, out hit, groundCheckRayLength))
+        Vector3 rayCastOrigin = transform.position;
+        Debug.DrawRay(rayCastOrigin, Vector3.up * headCheckRayLength, Color.red);
+        if (Physics.Raycast(rayCastOrigin, Vector3.up, out hit, headCheckRayLength))
         {
             if(checker.jumping == true)
             {
@@ -43,12 +99,23 @@ public class Raycasts : MonoBehaviour
             }
         }
     }
+    void HitCeiling(RaycastHit hit)
+    {
+        distanceToCeiling = hit.distance;
+        player.rb.velocity = new Vector3(player.rb.velocity.x, 0, 0);
+        if (distanceToCeiling >= 0 && distanceToCeiling <= 0.37f)
+        {
+            transform.position = new Vector3(hit.point.x, hit.point.y + 0.85f, 0);
+        }
+        distanceToCeiling = hit.distance;
+
+    }
+    //--------------------------------------------------------------------------------
     private void DownRays()
     {
-        rayCastOrigin = (transform.position - new Vector3(0, 0.45f, 0));
-
-        Debug.DrawRay(rayCastOrigin, Vector3.down * groundCheckRayLength, Color.red);
         RaycastHit hit;
+        Vector3 rayCastOrigin = transform.position - new Vector3(0, 0.45f, 0);
+        Debug.DrawRay(rayCastOrigin, Vector3.down * groundCheckRayLength, Color.red);
         if (Physics.Raycast(rayCastOrigin, Vector3.down, out hit, groundCheckRayLength))
         {
 
@@ -66,17 +133,6 @@ public class Raycasts : MonoBehaviour
             return;
         }
     }
-    void HitCeiling(RaycastHit hit)
-    {
-        distanceToCeiling = hit.distance;
-        player.rb.velocity = new Vector3(player.rb.velocity.x, 0, 0);
-        if (distanceToCeiling >= 0 && distanceToCeiling <= 0.37f)
-        {
-            transform.position = new Vector3(hit.point.x, hit.point.y + 0.85f, 0);
-        }
-        distanceToCeiling = hit.distance;
-
-    }
     void LandOnGround(RaycastHit hit)
     {
         distanceToGround = hit.distance;
@@ -89,4 +145,8 @@ public class Raycasts : MonoBehaviour
 
         player.grounded = true;
     }
+
+
+
+
 }
