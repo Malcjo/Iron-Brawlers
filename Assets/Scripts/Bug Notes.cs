85ba1fe68d0e2bab781bd9368fa26e09192ca6d0
Josh <joshuaeric.malcolm@gmail.com>
2020-11-26 11:58:18 +1300

    Deleted a couple of scripts

--- Assets/Scripts/Bug Notes.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BugNotes : MonoBehaviour
-{
-    //hold jump and punch breaks character, can't do anyting else while moving
-}
